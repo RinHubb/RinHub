f7c4dfdb6cdfcc6ea976751b8202fd7243a67822
RinHubb <158307995+RinHubb@users.noreply.github.com>
2024-02-02 20:01:42 +0700

    Update Rin_Hub_Auto_Farm.lua

--- Rin_Hub_Auto_Farm.lua
+++ Rin_Hub_Auto_Farm.lua
@@ -1,2 +1,2 @@
 setclipboard("https://discord.gg/DHdt8q78bR")
-game.Players.LocalPlayer:Kick("english : Join Discord and go to get script channel get new script | indonesia : Bergabunglah dengan Discord dan dapatkan saluran skrip dapatkan skrip baru | discord invite : https://discord.gg/DHdt8q78bR | copied discord invite!")
+game.Players.LocalPlayer:Kick("english:Join Discord to get new script | indonesia:Bergabunglah dengan Discord untuk mendapatkan skrip baru | discord invite : https://discord.gg/DHdt8q78bR | copied discord invite!")
