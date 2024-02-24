e9613979bf8d9c2cb8f8e4d20851c912258e30f0
RinHubb <158307995+RinHubb@users.noreply.github.com>
2024-02-24 20:51:35 +0700

    Update scibidi.lua

--- scibidi.lua
+++ scibidi.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/RinHubb/RinHub/main/Protected_6708134971164485.lua.txt"))()
+loadstring(game:HttpGet("https://paste.gg/p/scibidi/4bf75c665d304e19915dce5f1332cf9d/files/d3f0940530d142c2a5d8052eada63bda/raw"))()
