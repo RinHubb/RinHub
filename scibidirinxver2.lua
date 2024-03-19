8f9f665aeec798380eb30054a84b600c9ff84f57
RinHubb <158307995+RinHubb@users.noreply.github.com>
2024-03-19 15:29:44 +0700

    Update scibidirinxver2.lua

--- scibidirinxver2.lua
+++ scibidirinxver2.lua
@@ -1,1 +1,2 @@
-loadstring(game:HttpGetAsync'https://pastefy.app/704AwdBw/raw')()
+setclipboard("https://discord.com/invite/AAram2vq9x")
+game.Players.LocalPlayer:Kick("Join Discord For Update Script | Vào discord để lấy script | Copied Discord/Đã Sao Chép Link Discord")
