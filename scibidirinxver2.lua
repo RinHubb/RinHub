4ce6f8ac7ae488e38fb6663484fe66435bdb34a1
RinHubb <158307995+RinHubb@users.noreply.github.com>
2024-03-19 12:35:41 +0700

    Update scibidirinxver2.lua

--- scibidirinxver2.lua
+++ scibidirinxver2.lua
@@ -2,6 +2,4 @@ if _G.Language == "VietNam" then
 loadstring(game:HttpGetAsync'https://shz.al/~RinXScriptVietNam1')()
 elseif _G.Language == "English" then
 loadstring(game:HttpGetAsync'https://shz.al/~RinXEnglish')()
-elseif _G.Language == "" or _G.Language == nil then
-loadstring(game:HttpGetAsync'https://shz.al/~RinXEnglish')()
 end
